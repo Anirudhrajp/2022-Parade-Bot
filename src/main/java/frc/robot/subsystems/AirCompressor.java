9543b3d1458ef3415564b6f6e6fe75e0236fe8da
Anirudhrajp <78001283+Anirudhrajp@users.noreply.github.com>
2022-06-20 20:17:08 -0500

    6/20

--- src/main/java/frc/robot/subsystems/AirCompressor.java
+++ src/main/java/frc/robot/subsystems/AirCompressor.java
@@ -5,10 +5,42 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class AirCompressor extends SubsystemBase {
+  private Compressor compressor;
+  private double pressure;
+  private AnalogPotentiometer pressureAnalogPotentiometer;
+  private double psi;
+
   /** Creates a new AirCompressor. */
-  public AirCompressor() {}
+  public AirCompressor() {
+    compressor =  new Compressor(0, PneumaticsModuleType.CTREPCM);
+    pressureAnalogPotentiometer = new AnalogPotentiometer(Constants.kAnalogInPort, Constants.kscale, Constants.koffset);
+  }
+
+  
+  public void compressorTurnOn() {
+    compressor.enableDigital();
+  }
+
+  public void compressorTurnOff() {
+    compressor.disable();
+  }
+
+  public double getPressure() {
+    pressure = compressor.getPressure();
+    return pressure;
+  }
+
+  public double getPsi() {
+    psi = pressureAnalogPotentiometer.get();
+    return psi;
+  }
+
 
   @Override
   public void periodic() {
